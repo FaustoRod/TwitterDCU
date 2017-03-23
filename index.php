babdd18d990450c1268e0a4a54f0dffd8d17fdcf
FaustoRod <faustoelnike@gmail.com>
2017-03-23 06:55:09 -0400

    Add

--- index.php
+++ index.php
@@ -5,33 +5,29 @@
 <html>
 
 	<head>
-		<title>ReTech</title>
-		<link rel="stylesheet" href="style.css">
+		<title>FreePost</title>
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
 	</head>
 	
 	<body>
 		<header>
 			<div class="header-main">			
-				<div class="header-item logo-cont">
-					<div class="logo">
-						<img src="assets/logo.png" style="width:100%;height:100%;">
-					</div>
+				<div class="header-item">
+				
+				</div>
 
-					<div class="titulo">
-					<h1>My-Tech</h1>
-					</div>
+				<div class="header-item logo">
 				</div>
 
 				<div class="header-item">
-				</div>
-
-				<div class="header-item">
-					redes
+					
 				</div>
 			</div>
 		</head>
 		<script type="text/javascript" src="JavaScript/javascript.js"></script>
 		<script type="text/javascript" src="JavaScript/jquery-3.1.1.min.js"></script>
+		<script type="text/javascript" src="JavaScript/bootstrap.min.js"></script>
 	</body>
 
 </html>
