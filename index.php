1817a9612c02b55cab8a55e58fc16a1532db4604
FaustoRod <faustoelnike@gmail.com>
2017-03-23 05:43:04 -0400

    Archivos Agregados
    Se agregaron los archivos necesarios

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>ReTech</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	
+	<body>
+		<header>
+			<div class="header-main">			
+				<div class="header-item logo-cont">
+					<div class="logo">
+						<img src="assets/logo.png" style="width:100%;height:100%;">
+					</div>
+
+					<div class="titulo">
+					<h1>My-Tech</h1>
+					</div>
+				</div>
+
+				<div class="header-item">
+				</div>
+
+				<div class="header-item">
+					redes
+				</div>
+			</div>
+		</head>
+		<script type="text/javascript" src="JavaScript/javascript.js"></script>
+		<script type="text/javascript" src="JavaScript/jquery-3.1.1.min.js"></script>
+	</body>
+
+</html>
